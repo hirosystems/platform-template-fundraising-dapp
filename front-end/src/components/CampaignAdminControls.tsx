b8f371c76d8896ccff223cb0d85fe67cc5a79941
Leah McBeth <leahloughran@gmail.com>
2025-02-21 13:29:43 -0700

    UI update

--- front-end/src/components/CampaignAdminControls.tsx
+++ front-end/src/components/CampaignAdminControls.tsx
@@ -77,6 +77,7 @@ export default function CampaignAdminControls({
   };
 
   const handleCancel = async () => {
+    setIsCancelConfirmationModalOpen(false);
     const txOptions = getCancelTx(
       getStacksNetworkString(),
       currentWalletAddress || ""
